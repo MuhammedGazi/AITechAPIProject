e8f50454a18f5a30a66ed5b0a98912ede6addf50
MuhammedGazi <muhammed.celenk1@ogr.sakarya.edu.tr>
2025-11-20 22:31:55 +0300

    category serviceler eklendi controller yapıldı

---
+++ AITech.API/Controllers/CategoriesController.cs
@@ -0,0 +1,47 @@
+﻿using AITech.Business.Services.CategoryServices;
+using AITech.DTO.CategoryDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AITech.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController(ICategoryService _categoryService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.TGetAllAsync();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var category = await _categoryService.TGetByIdAsync(id);
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCategoryDto dto)
+        {
+            await _categoryService.TCreateAsync(dto);
+            return Ok("kategori oluşturuldu");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _categoryService.TDeleteAsync(id);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> EditCategory(UpdateCategoryDto dto)
+        {
+            await _categoryService.TUpdateAsync(dto);
+            return Ok();
+        }
+
+    }
+}
