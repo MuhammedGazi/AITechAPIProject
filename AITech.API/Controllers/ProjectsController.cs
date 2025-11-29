9558b6a22999de1c33152affb1057b172161255d
MuhammedGazi <muhammed.celenk1@ogr.sakarya.edu.tr>
2025-11-29 16:49:58 +0300

    project

---
+++ AITech.API/Controllers/ProjectsController.cs
@@ -0,0 +1,60 @@
+﻿using AITech.Business.Services.ProjectServices;
+using AITech.DTO.ProjectDtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AITech.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController(IProjectService _projectService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var project=await  _projectService.TGetAllAsync();
+            return Ok(project);
+        }
+
+        [HttpGet("WithCategories")]
+        public async Task<IActionResult> GetAllWithCategories()
+        {
+            var  projects=await  _projectService.TGetProjectWithCategoriesAsync();
+            return Ok(projects);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult>  GetById(int id)
+        {
+            var project=await _projectService.TGetByIdAsync(id);
+            if(project is null)
+            {
+                return BadRequest("proje bulunamadı");
+            }
+            return Ok(project);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>  Create(CreateProjectDto projectDto)
+        {
+            await _projectService.TCreateAsync(projectDto);
+            return Created();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateProjectDto projectDto)
+        {
+            await  _projectService.TUpdateAsync(projectDto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _projectService.TDeleteAsync(id);
+            return NoContent();
+        }
+
+    }
+}
---
+++ AITech.Business/Services/ProjectServices/ProjectService.cs
@@ -0,0 +1,54 @@
+﻿using AITech.DataAccess.Repositories.ProjectRepositories;
+using AITech.DataAccess.UnitOfWorks;
+using AITech.DTO.ProjectDtos;
+using AITech.Entity.Entities;
+using Mapster;
+
+namespace AITech.Business.Services.ProjectServices
+{
+    public class ProjectService(IProjectRepository _repository, IUnitOfWork _unitOfWork) : IProjectService
+    {
+        public async Task TCreateAsync(CreateProjectDto createDto)
+        {
+            var project = createDto.Adapt<Project>();
+            await _repository.CreateAsync(project);
+            await _unitOfWork.SaveChangesAsync();
+        }
+
+        public async Task TDeleteAsync(int id)
+        {
+            var project = await _repository.GetByIdAsync(id);
+            if (project == null)
+            {
+                throw new Exception("proje bulunamadı");
+            }
+            _repository.Delete(project);
+            await _unitOfWork.SaveChangesAsync();
+        }
+
+        public async Task<List<ResultProjectDto>> TGetAllAsync()
+        {
+            var projects = await _repository.GetAllAsync();
+            return projects.Adapt<List<ResultProjectDto>>();
+        }
+
+        public async Task<ResultProjectDto> TGetByIdAsync(int id)
+        {
+            var project = await _repository.GetByIdAsync(id);
+            return project.Adapt<ResultProjectDto>();
+        }
+
+        public async Task<List<ResultProjectDto>> TGetProjectWithCategoriesAsync()
+        {
+            var project = await _repository.GetProjectWithCategoriesAsync();
+            return project.Adapt<List<ResultProjectDto>>();
+        }
+
+        public async Task TUpdateAsync(UpdateProjectDto updateDto)
+        {
+            var project = updateDto.Adapt<Project>();
+            _repository.Update(project);
+            await _unitOfWork.SaveChangesAsync();
+        }
+    }
+}
