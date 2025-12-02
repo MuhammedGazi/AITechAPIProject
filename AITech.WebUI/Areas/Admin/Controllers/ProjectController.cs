6e5fb7d917b015702c19166ede28a1c9ab3cd404
MuhammedGazi <muhammed.celenk1@ogr.sakarya.edu.tr>
2025-12-02 22:00:07 +0300

    project web uı

---
+++ AITech.WebUI/Areas/Admin/Controllers/ProjectController.cs
@@ -0,0 +1,67 @@
+﻿using AITech.WebUI.DTOs.ProjectDtos;
+using AITech.WebUI.Services.CategoryServices;
+using AITech.WebUI.Services.ProjectServices;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AITech.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProjectController(IProjectService _projectService, ICategoryService _categoryService) : Controller
+    {
+        private async Task GetCategoriesAsync()
+        {
+            var categoryList = await _categoryService.GetAllAsync();
+            ViewBag.categories = (from category in categoryList
+                                  select new SelectListItem
+                                  {
+                                      Text = category.Name,
+                                      Value = category.Id.ToString()
+                                  }).ToList();
+        }
+        public async Task<IActionResult> Index()
+        {
+            var projects = await _projectService.GetAllAsync();
+            return View(projects);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateProject()
+        {
+            await GetCategoriesAsync();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProject(CreateProjectDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                await GetCategoriesAsync();
+                return View(dto);
+            }
+            await _projectService.CreateAsync(dto);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateProject(int id)
+        {
+            await GetCategoriesAsync();
+            var project = await _projectService.GetByIdAsync(id);
+            return View(project);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateProject(UpdateProjectDto dto)
+        {
+            await _projectService.UpdateAsync(dto);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> DeleteProject(int id)
+        {
+            await _projectService.DeleteAsync(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ AITech.WebUI/Validators/ProjectValidator.cs
@@ -0,0 +1,16 @@
+﻿using AITech.WebUI.DTOs.ProjectDtos;
+using FluentValidation;
+using Microsoft.AspNetCore.Rewrite;
+
+namespace AITech.WebUI.Validators
+{
+    public class ProjectValidator: AbstractValidator<CreateProjectDto>
+    {
+        public ProjectValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("başlık boş bırakılamaz");
+            RuleFor(x => x.ImageUrl).NotEmpty().WithMessage("resim boş bırakılamaz");
+            RuleFor(x => x.CategoryId).NotEmpty().WithMessage("kategori boş bırakılamaz");
+        }
+    }
+}
